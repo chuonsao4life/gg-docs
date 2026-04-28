e15b85bd6fb383336f62bb16c1bc287deb5cf206
DoanDieuLinh <ldieu0604@gmail.com>
2026-04-28 17:55:20 +0700

    clean: remove old backend files and large modules

--- backend/src/task-boards/dto/update-task-board.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTaskBoardDto } from './create-task-board.dto';
-
-export class UpdateTaskBoardDto extends PartialType(CreateTaskBoardDto) {}
--- backend/src/task-boards/task-boards.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TaskBoardsService } from './task-boards.service';
-import { TaskBoardsController } from './task-boards.controller';
-
-@Module({
-  controllers: [TaskBoardsController],
-  providers: [TaskBoardsService],
-})
-export class TaskBoardsModule {}
